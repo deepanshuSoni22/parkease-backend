f1c74e966de36e675fe8c8169839f32e01ab1922
deepanshu <deepanshusoni022@gmail.com>
2026-05-09 21:10:22 +0000

    initial project setup

---
+++ src/test/java/org/example/park_ease/ParkEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.park_ease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
