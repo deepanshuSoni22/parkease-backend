ea9c786b6d98f2e591a5918fe49627c4613d6408
deepanshu <deepanshusoni022@gmail.com>
2026-05-22 14:55:10 +0000

    fix trailing slash in frontend url

--- src/main/java/org/example/park_ease/config/ProdSecurityConfig.java
+++ src/main/java/org/example/park_ease/config/ProdSecurityConfig.java
@@ -137,7 +137,7 @@ public class ProdSecurityConfig {
     public CorsConfigurationSource prodCors() {
 
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("https://deepanshusoni22.github.io/"));
+        cfg.setAllowedOrigins(List.of("https://deepanshusoni22.github.io"));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("Content-Type", "X-XSRF-TOKEN"));
         cfg.setAllowCredentials(true);
