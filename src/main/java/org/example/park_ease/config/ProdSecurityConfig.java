0fec71af2af0666b099071eb8585a44e53b6755c
deepanshu <deepanshusoni022@gmail.com>
2026-05-26 07:29:10 +0000

    fix again: update frontend url back to original working one

--- src/main/java/org/example/park_ease/config/ProdSecurityConfig.java
+++ src/main/java/org/example/park_ease/config/ProdSecurityConfig.java
@@ -142,7 +142,7 @@ public class ProdSecurityConfig {
     public CorsConfigurationSource prodCors() {
 
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("https://deepanshuSoni22.github.io"));
+        cfg.setAllowedOrigins(List.of("https://deepanshusoni22.github.io"));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
